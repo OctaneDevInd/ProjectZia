d8898a634db9106b777e291039b570d40a66f3a3
OctaneDevInd <OctaneDevInd@users.noreply.github.com>
2019-03-05 16:58:04 +0530

    Add project files.

---
+++ ProjectZia/ProjectZia/ListenPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+
+namespace ProjectZia
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ListenPage : ContentPage
+	{
+		public ListenPage ()
+		{
+			InitializeComponent ();
+		}
+
+        private void StartListeningButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ ProjectZia/ProjectZia/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ProjectZia
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ListenButton_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new ListenPage());
+        }
+
+        private void TalkButton_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new TalkPage());
+        }
+
+        private void ContentPage_Appearing(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
