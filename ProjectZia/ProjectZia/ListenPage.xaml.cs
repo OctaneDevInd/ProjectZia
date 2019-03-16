1950854041689a87f7fd54bc7d6da411476d301d
OctaneDevInd <OctaneDevInd@users.noreply.github.com>
2019-03-16 10:03:33 +0530

    Included Plugin.AudioRecorder and added code for recording audio.

--- ProjectZia/ProjectZia/ListenPage.xaml.cs
+++ ProjectZia/ProjectZia/ListenPage.xaml.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
-
+using Plugin.AudioRecorder;
 using Pocketsphinx;
 
 
@@ -16,60 +16,122 @@ namespace ProjectZia
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class ListenPage : ContentPage
 	{
-        Pocketsphinx.Decoder d;
-        Config c;
+        //Pocketsphinx.Decoder d;
+        //Config c;
+        AudioRecorderService recorder;
+        AudioPlayer player;
         public ListenPage ()
 		{
 			InitializeComponent ();
-            try
-            {
-                c = Pocketsphinx.Decoder.DefaultConfig();
-                c.SetString("-hmm", "/models/en-us/mdef");
-                c.SetString("-dict", "/models/dict/cmudict-en-us.dict");
-                c.SetFloat("-kws_threshold", 1e-30);
+            //try
+            //{
+            //    c = Pocketsphinx.Decoder.DefaultConfig();
+            //    c.SetString("-hmm", "/models/en-us/mdef");
+            //    c.SetString("-dict", "/models/dict/cmudict-en-us.dict");
+            //    c.SetFloat("-kws_threshold", 1e-30);
 
-                c.SetFloat("-samprate", 44100);
-                c.SetInt("-nfft", 2048);
+            //    c.SetFloat("-samprate", 44100);
+            //    c.SetInt("-nfft", 2048);
 
-                d = new Pocketsphinx.Decoder(c);
-            }catch(Exception ex)
+            //    d = new Pocketsphinx.Decoder(c);
+            //}catch(Exception ex)
+            //{
+            //    DisplayAlert("Oops!", ex.Message, "OK");
+            //}
+            recorder = new AudioRecorderService
             {
-                DisplayAlert("Oops!", ex.Message, "OK");
-            }
+                StopRecordingAfterTimeout = true,
+                AudioSilenceTimeout = TimeSpan.FromSeconds(2),
+                TotalAudioTimeout = TimeSpan.FromSeconds(20)
+            };
+            player = new AudioPlayer();
+            player.FinishedPlaying += Player_FinishedPlaying;
         }
 
-        static byte[] convertToBytes(float[] data, int channels)
+        private void Player_FinishedPlaying(object sender, EventArgs e)
         {
-            float tot = 0;
-            byte[] byteData = new byte[data.Length / channels * 2];
-            for (int i = 0; i < data.Length / channels; i++)
+            playRecordingButton.IsEnabled = true;
+            startListeningButton.IsEnabled = true;
+        }
+
+        //static byte[] convertToBytes(float[] data, int channels)
+        //{
+        //    float tot = 0;
+        //    byte[] byteData = new byte[data.Length / channels * 2];
+        //    for (int i = 0; i < data.Length / channels; i++)
+        //    {
+        //        float sum = 0;
+        //        for (int j = 0; j < channels; j++)
+        //        {
+        //            sum += data[i * channels + j];
+        //        }
+        //        tot += sum * sum;
+        //        short val = (short)(sum / channels * 20000); // volume
+        //        byteData[2 * i] = (byte)(val & 0xff);
+        //        byteData[2 * i + 1] = (byte)(val >> 8);
+        //    }
+        //    //Debug.Log (Math.Sqrt(tot / data.Length / channels));
+        //    return byteData;
+        //}
+
+        private async void StartListeningButton_Clicked(object sender, EventArgs e)
+        {
+
+            await RecordAudio();
+
+        }
+        private async Task RecordAudio()
+        {
+            try
             {
-                float sum = 0;
-                for (int j = 0; j < channels; j++)
+                if(!recorder.IsRecording)
                 {
-                    sum += data[i * channels + j];
-                }
-                tot += sum * sum;
-                short val = (short)(sum / channels * 20000); // volume
-                byteData[2 * i] = (byte)(val & 0xff);
-                byteData[2 * i + 1] = (byte)(val >> 8);
-            }
-            //Debug.Log (Math.Sqrt(tot / data.Length / channels));
-            return byteData;
-        }
+                    playRecordingButton.IsEnabled = false;
 
-        private void StartListeningButton_Clicked(object sender, EventArgs e)
-        {
-            try
-            {
-                d.StartUtt();
+                    var audiorecordTask = await recorder.StartRecording();
+                    startListeningButton.Text = "Stop Listening";
+
+                    await audiorecordTask;
+                    startListeningButton.Text = "Start Listening";
+                    playRecordingButton.IsEnabled = true;
+                }
+                else
+                {
+                    startListeningButton.IsEnabled = false;
+                    await recorder.StopRecording();
+                    startListeningButton.IsEnabled = true;
+                }
             }
             catch(Exception ex)
             {
-                DisplayAlert("Exception",ex.Message,"OK");
+                await DisplayAlert("Error occured in recording audio.", ex.Message, "OK");
             }
+        }
 
+        private void PlayRecordingButton_Clicked(object sender, EventArgs e)
+        {
+             PlayAudio();
+        }
 
+        private void PlayAudio()
+        {
+            try
+            {
+                var filePath = recorder.GetAudioFilePath();
+
+                if (filePath != null)
+                {
+                    playRecordingButton.IsEnabled = false;
+                    startListeningButton.IsEnabled = false;
+
+                    player.Play(filePath);
+                }
+            }
+            catch (Exception ex)
+            {
+                //blow up the app!
+                throw ex;
+            }
         }
     }
 }
